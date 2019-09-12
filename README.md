e3da31e20c4a923da404c4e2787ca9fad72a4ac6
anujsharma-bbd <sharmaanuj@hotmail.com>
2019-09-12 15:33:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-with-usestate
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-with-usestate)
---
+++ index.js
@@ -0,0 +1,46 @@
+import React, { Component, useState,useEffect } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+function Header(){
+  const [name,setName]= useState('anuj');
+  useEffect(()=>{
+    new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        resolve('my new name');
+      },10000);
+    }).then(response=>{
+      setName(response);
+    })
+  })
+ return (
+   <>
+   <h1>this is header</h1>
+   <span>{name}</span> 
+   <button onClick={()=> setName( name + 'sharmaa' )}>ClickMe</button>
+   </>
+ );
+}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+      <Header />
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
